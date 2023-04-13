c293d2d276af4846c35ead85627986e9fb5a416b
Alexey Polushin <a.polushin@redkit-lab.ru>
2023-04-13 20:11:14 +0500

    Use windows root certs for grpc

--- investapiclient.cpp
+++ investapiclient.cpp
@@ -10,9 +10,59 @@
 #include "stopordersservice.h"
 #include "commontypes.h"
 
+#ifdef _WIN32
+#include <Windows.h>
+#include <wincrypt.h>
+
+std::string utf8Encode(const std::wstring& wstr)
+{
+	if (wstr.empty())
+		return std::string();
+
+	int sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
+	std::string strTo(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], sizeNeeded, NULL, NULL);
+	return strTo;
+}
+
+grpc::SslCredentialsOptions getSslOptions()
+{
+	// Fetch root certificate as required on Windows (s. issue 25533).
+	grpc::SslCredentialsOptions result;
+
+	// Open root certificate store.
+	HANDLE hRootCertStore = CertOpenSystemStoreW(NULL, L"ROOT");
+	if (!hRootCertStore)
+		return result;
+
+	// Get all root certificates.
+	PCCERT_CONTEXT pCert = NULL;
+	while ((pCert = CertEnumCertificatesInStore(hRootCertStore, pCert)) != NULL)
+	{
+		// Append this certificate in PEM formatted data.
+		DWORD size = 0;
+		CryptBinaryToStringW(pCert->pbCertEncoded, pCert->cbCertEncoded, CRYPT_STRING_BASE64HEADER, NULL, &size);
+		std::vector<WCHAR> pem(size);
+		CryptBinaryToStringW(pCert->pbCertEncoded, pCert->cbCertEncoded, CRYPT_STRING_BASE64HEADER, pem.data(), &size);
+
+		result.pem_root_certs += utf8Encode(pem.data());
+	}
+	CertCloseStore(hRootCertStore, 0);
+
+	return result;
+}
+#endif
+
 InvestApiClient::InvestApiClient(const std::string &host, const std::string &pass)
 {
-    auto channel = grpc::CreateChannel(host, grpc::SslCredentials(grpc::SslCredentialsOptions()));
+    auto channel = grpc::CreateChannel(host, grpc::SslCredentials(
+#ifdef _WIN32
+       getSslOptions()
+#else
+       grpc::SslCredentialsOptions()
+#endif
+    ));
+
     m_services["sandbox"] = std::make_shared<Sandbox>(channel, pass);
     m_services["users"] = std::make_shared<Users>(channel, pass);
     m_services["marketdata"] = std::make_shared<MarketData>(channel, pass);    

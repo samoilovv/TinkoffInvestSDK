086996e680c7d4bb6f098e3111b820fbc9edb7c0
v.samoilov <samoilov@dr-cr.ru>
2023-06-02 21:02:09 +0300

    Fix linux build

--- cmake/fix_keywords.sh
+++ cmake/fix_keywords.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-#sed -i -- 's/namespace public {/namespace p_ublic_ {/g' $1/*.pb.h $1/*.pb.cc
-#sed -i -- 's/tinkoff::public::/tinkoff::p_ublic_::/g' $1/*.pb.h $1/*.pb.cc
+#sed -i -- 's/namespace public {/namespace public_ {/g' $1/*.pb.h $1/*.pb.cc
+#sed -i -- 's/tinkoff::public::/tinkoff::public_::/g' $1/*.pb.h $1/*.pb.cc
 
-sed -i -- 's/namespace public {/namespace public_ {/g' $OUTPUT_PB_SOURCE $OUTPUT_PB_HEADER $OUTPUT_GRPC_SOURCE $OUTPUT_GRPC_HEADER
-sed -i -- 's/tinkoff::public::/tinkoff::public_::/g' $OUTPUT_PB_SOURCE $OUTPUT_PB_HEADER $OUTPUT_GRPC_SOURCE $OUTPUT_GRPC_HEADER
+#sed -i -- 's/namespace public {/namespace public_ {/g' $OUTPUT_PB_SOURCE $OUTPUT_PB_HEADER $OUTPUT_GRPC_SOURCE $OUTPUT_GRPC_HEADER
+#sed -i -- 's/tinkoff::public::/tinkoff::public_::/g' $OUTPUT_PB_SOURCE $OUTPUT_PB_HEADER $OUTPUT_GRPC_SOURCE $OUTPUT_GRPC_HEADER
+
+sed -i -- 's/namespace public {/namespace public_ {/g' $1 $2 $3 $4
+sed -i -- 's/tinkoff::public::/tinkoff::public_::/g' $1 $2 $3 $4
